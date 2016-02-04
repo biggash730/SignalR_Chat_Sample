efc07a9ce5606cb617e698e798717e91fe377144
Francis Miah <francis@axoninfosystems.com>
2016-02-04 11:27:30 +0000

    initial commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Pust some logic here
+            FakePush(name, message);
+            PushUsers(name);
+        }
+
+        public void FakePush(string name, string message)
+        {
+
+            Clients.All.addNewMessageToPage(name, message);
+        }
+
+        public void PushUsers(string name)
+        {
+
+            Clients.All.pushUsersToPage(name);
+        }
+    }
+}
